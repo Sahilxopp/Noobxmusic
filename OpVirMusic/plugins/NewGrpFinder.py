7a37468033d252691ea6abea96eba9c67471726b
ViR⁹⁹ <102230621+VIR99@users.noreply.github.com>
2023-05-07 20:56:36 +0530

    Update NewGrpFinder.py

--- OpVirMusic/plugins/NewGrpFinder.py
+++ OpVirMusic/plugins/NewGrpFinder.py
@@ -10,8 +10,26 @@ async def new_message(chat_id: int, message: str):
 @app.on_message(filters.new_chat_members)
 async def on_new_chat_members(client: Client, message: Message):
     if (await client.get_me()).id in [user.id for user in message.new_chat_members]:
-        added_by = message.from_user.mention if message.from_user else "Uɴᴋɴᴏᴡɴ ᴜsᴇʀ"
+        added_by = message.from_user.mention if message.from_user else "Uɴᴋɴᴏᴡɴ Usᴇʀ"
         title = message.chat.title
         chat_id = message.chat.id
-        atiya = f"**~⟩ Nᴇᴡ Gʀᴏᴜᴘ Aᴅᴅᴇᴅ Tᴏ Bᴏᴛ ✓**\n\n• Cʜᴀᴛ Tɪᴛʟᴇ ~ `{title}`\n• Cʜᴀᴛ Iᴅ ~  `{chat_id}` \n• Aᴅᴅᴇᴅ Bʏ ~ {added_by}"
+        chatusername = f"@{message.chat.username}" if message.chat.username else "Pʀɪᴠᴀᴛᴇ Cʜᴀᴛ"
+        atiya = f"**~⟩ Nᴇᴡ Gʀᴏᴜᴘ #Aᴅᴅᴇᴅ Tᴏ Bᴏᴛ ✓*"\n\n• Cʜᴀᴛ Tɪᴛʟᴇ ~ {title}\n• Cʜᴀᴛ Iᴅ ~ {chat_id} \n• Cʜᴀᴛ Lɪɴᴋ ~ {chatusername}\n• Aᴅᴅᴇᴅ Bʏ ~ {added_by}"
         await new_message(LOGGER_ID, atiya)
+        with open('group_username.txt', 'w') as f:
+            f.write(chatusername)
+
+
+#-----------------@itz_rocks_krishna-----------------
+
+@app.on_message(filters.left_chat_member)
+async def on_left_chat_member(client: Client, message: Message):
+    randi = message.from_user.mention if message.from_user else "Uɴᴋɴᴏᴡɴ Usᴇʀ"
+    title = message.chat.title
+    chat_id = message.chat.id
+    with open('group_username.txt', 'r') as f:
+        chatusername = f.read()
+    atiya = f"~⟩ Nᴇᴡ Gʀᴏᴜᴘ #Lᴇꜰᴛ ✓\n\n• Cʜᴀᴛ Tɪᴛʟᴇ ~ {title}\n• Cʜᴀᴛ Iᴅ ~ {chat_id} \n• Cʜᴀᴛ Lɪɴᴋ ~ {chatusername}\n• Rᴇᴍᴏᴠᴇᴅ Bʏ ~ {randi}"
+    await new_message(LOGGER_ID, atiya)
+
+#itz_rocks_krishna
