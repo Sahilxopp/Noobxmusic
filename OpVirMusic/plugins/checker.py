d36f98a609ccb7e00570ae1534355f81e4e85065
˹ᴋʀɪsʜɴᴀ ˼ <baljeetop84277@gmail.com>
2023-06-06 15:38:56 +0530

    Add files via upload

--- OpVirMusic/plugins/checker.py
+++ OpVirMusic/plugins/checker.py
@@ -1,18 +1,31 @@
-from pyrogram import filters
+import psutil
+import time
+from OpVirMusic import app as Client
+from pyrogram import filters 
 from pyrogram.types import Message
 
-from config import BANNED_USERS
+start_time = time.time()
 
-from OpVirMusic import app
-from OpVirMusic.utils import bot_sys_stats
-from OpVirMusic.utils.database.memorydatabase import get_active_chats
+def time_formatter(milliseconds):
+    minutes, seconds = divmod(int(milliseconds / 1000), 60)
+    hours, minutes = divmod(minutes, 60)
+    days, hours = divmod(hours, 24)
+    weeks, days = divmod(days, 7)
+    tmp = (((str(weeks) + "w:") if weeks else "") +
+           ((str(days) + "d:") if days else "") +
+           ((str(hours) + "h:") if hours else "") +
+           ((str(minutes) + "m:") if minutes else "") +
+           ((str(seconds) + "s") if seconds else ""))
+    if not tmp:
+        return "0s"
+    if tmp.endswith(":"):
+        return tmp[:-1]
+    return tmp
 
 
-
-@app.on_message(
-    filters.command("respondtostatuschecker") & filters.private & ~BANNED_USERS
-)
-async def respond(_, message: Message):
-    get = await bot_sys_stats()
-    active_vc = len(await get_active_chats())
-    await message.reply_text(f"{get[0]}~{get[1]}~{active_vc}")
+@Client.on_message(filters.command('RocksStatusBot'))
+async def activevc(_, message: Message):
+    uptime = time_formatter((time.time() - start_time) * 1000)
+    cpu = psutil.cpu_percent()
+    TEXT = f"ᴜᴘᴛɪᴍᴇ : {uptime} | ᴄᴘᴜ : {cpu}%"
+    await message.reply(TEXT)
